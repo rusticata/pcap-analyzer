8c2398778e8a947160ac1c2a23f3845754e2dfdd
Pierre Chifflier <chifflier@wzdftpd.net>
2019-04-25 17:31:10 +0200

    Rename lifetimes and document l4_data field

--- libpcap-analyzer/src/packet_data.rs
+++ libpcap-analyzer/src/packet_data.rs
@@ -1,14 +1,15 @@
 use crate::five_tuple::FiveTuple;
 use crate::flow::Flow;
 
-pub struct PacketData<'a, 't, 'f> {
+pub struct PacketData<'l3, 'l4, 't, 'f> {
     pub five_tuple: &'t FiveTuple,
     /// true if this packet is in same direction as the first packet
     /// seen in this flow
     pub to_server: bool,
     pub l3_type: u16,
-    pub l3_data: &'a [u8],
+    pub l3_data: &'l3 [u8],
     pub l4_type: u8,
-    pub l4_data: Option<&'a [u8]>,
+    /// L4 data, if protocol is known by core engine
+    pub l4_data: Option<&'l4 [u8]>,
     pub flow: Option<&'f Flow>,
 }
