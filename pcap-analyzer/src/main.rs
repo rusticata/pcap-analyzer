9038962f380d90633dd0dfab630921434dd65b89
Pierre Chifflier <chifflier@wzdftpd.net>
2019-05-08 16:31:09 +0200

    rustfmt

--- pcap-analyzer/src/main.rs
+++ pcap-analyzer/src/main.rs
@@ -2,7 +2,7 @@
 extern crate log;
 
 extern crate clap;
-use clap::{Arg,App,crate_version};
+use clap::{crate_version, App, Arg};
 
 extern crate env_logger;
 extern crate flate2;
@@ -15,10 +15,10 @@ use std::path::Path;
 use flate2::read::GzDecoder;
 use xz2::read::XzDecoder;
 
-use libpcap_analyzer::{plugins,Analyzer};
+use libpcap_analyzer::{plugins, Analyzer};
 use libpcap_tools::{Config, PcapEngine};
 
-fn load_config(config:&mut Config, filename:&str) -> Result<(),io::Error> {
+fn load_config(config: &mut Config, filename: &str) -> Result<(), io::Error> {
     debug!("Loading configuration {}", filename);
     let path = Path::new(&filename);
     let file = File::open(path)?;
@@ -26,75 +26,89 @@ fn load_config(config:&mut Config, filename:&str) -> Result<(),io::Error> {
 }
 
 fn main() -> io::Result<()> {
-   let matches = App::new("Pcap analyzer")
+    let matches = App::new("Pcap analyzer")
         .version(crate_version!())
         .author("Pierre Chifflier")
         .about("Tool for Pcap file analyzis")
-        .arg(Arg::with_name("verbose")
-             .help("Be verbose")
-             .short("v")
-             .long("verbose"))
-        .arg(Arg::with_name("plugins")
-             .help("Plugins to load (default: all)")
-             .short("p")
-             .long("plugins")
-             .takes_value(true))
-        .arg(Arg::with_name("config")
-             .help("Configuration file")
-             .short("c")
-             .long("config")
-             .takes_value(true))
-        .arg(Arg::with_name("INPUT")
-             .help("Input file name")
-             .required(true)
-             .index(1))
+        .arg(
+            Arg::with_name("verbose")
+                .help("Be verbose")
+                .short("v")
+                .long("verbose"),
+        )
+        .arg(
+            Arg::with_name("plugins")
+                .help("Plugins to load (default: all)")
+                .short("p")
+                .long("plugins")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("config")
+                .help("Configuration file")
+                .short("c")
+                .long("config")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Input file name")
+                .required(true)
+                .index(1),
+        )
         .get_matches();
 
-   env_logger::init();
+    env_logger::init();
 
-   debug!("Pcap analyser {}", crate_version!());
+    debug!("Pcap analyser {}", crate_version!());
 
-   let builder = plugins::plugins_factory();
-   let mut config = Config::default();
-   if let Some(filename) = matches.value_of("config") {
-       load_config(&mut config, filename)?;
-   }
-   let mut plugins = plugins::build_plugins(&builder,&config);
-   if let Some(plugin_names) = matches.value_of("plugins") {
-       debug!("Restricting plugins to: {}", plugin_names);
-       let names : Vec<_> = plugin_names.split(",").collect();
-       plugins.storage.retain(|k, _| {
-           names.iter().any(|&x| k.contains(x))
-       });
-   }
+    let builder = plugins::plugins_factory();
+    let mut config = Config::default();
+    if let Some(filename) = matches.value_of("config") {
+        load_config(&mut config, filename)?;
+    }
+    let mut plugins = plugins::build_plugins(&builder, &config);
+    if let Some(plugin_names) = matches.value_of("plugins") {
+        debug!("Restricting plugins to: {}", plugin_names);
+        let names: Vec<_> = plugin_names.split(",").collect();
+        plugins
+            .storage
+            .retain(|k, _| names.iter().any(|&x| k.contains(x)));
+    }
 
-   debug!("  Plugins loaded: {}", plugins.storage.len());
-   debug!("  Plugins: {}", plugins.storage.keys().map(|s| s.as_ref()).collect::<Vec<_>>().join(", "));
-   if plugins.storage.is_empty() {
-       warn!("No plugins loaded");
-   }
-   let analyzer = Analyzer::new(plugins);
-   let mut engine = PcapEngine::new(Box::new(analyzer), &config);
+    debug!("  Plugins loaded: {}", plugins.storage.len());
+    debug!(
+        "  Plugins: {}",
+        plugins
+            .storage
+            .keys()
+            .map(|s| s.as_ref())
+            .collect::<Vec<_>>()
+            .join(", ")
+    );
+    if plugins.storage.is_empty() {
+        warn!("No plugins loaded");
+    }
+    let analyzer = Analyzer::new(plugins);
+    let mut engine = PcapEngine::new(Box::new(analyzer), &config);
 
-   let input_filename = matches.value_of("INPUT").unwrap();
-   // let verbose = matches.is_present("verbose");
+    let input_filename = matches.value_of("INPUT").unwrap();
 
-   let mut input_reader =
-       if input_filename == "-" {
-           Box::new(io::stdin()) as Box<io::Read>
-       } else {
-           let path = Path::new(&input_filename);
-           let file = File::open(path)?;
-           if input_filename.ends_with(".gz") {
-               Box::new(GzDecoder::new(file)) as Box<io::Read>
-           } else if input_filename.ends_with(".xz") {
-               Box::new(XzDecoder::new(file)) as Box<io::Read>
-           } else {
-               Box::new(file)
-           }
-       };
+    let mut input_reader = if input_filename == "-" {
+        Box::new(io::stdin()) as Box<io::Read>
+    } else {
+        let path = Path::new(&input_filename);
+        let file = File::open(path)?;
+        if input_filename.ends_with(".gz") {
+            Box::new(GzDecoder::new(file)) as Box<io::Read>
+        } else if input_filename.ends_with(".xz") {
+            Box::new(XzDecoder::new(file)) as Box<io::Read>
+        } else {
+            Box::new(file)
+        }
+    };
 
-   let _ = engine.run(&mut input_reader).expect("run analyzer");
+    let _ = engine.run(&mut input_reader).expect("run analyzer");
 
-   Ok(())
+    Ok(())
 }
--- pcap-rewrite/src/main.rs
+++ pcap-rewrite/src/main.rs
@@ -2,12 +2,12 @@
 extern crate log;
 
 extern crate clap;
-use clap::{Arg,App,crate_version};
+use clap::{crate_version, App, Arg};
 
 // extern crate env_logger;
-extern crate std_logger;
 extern crate flate2;
 extern crate pcap_parser;
+extern crate std_logger;
 extern crate xz2;
 
 use std::fs::File;
@@ -22,7 +22,7 @@ use libpcap_tools::{Config, PcapEngine};
 mod rewriter;
 use crate::rewriter::*;
 
-fn load_config(config:&mut Config, filename:&str) -> Result<(),io::Error> {
+fn load_config(config: &mut Config, filename: &str) -> Result<(), io::Error> {
     debug!("Loading configuration {}", filename);
     let path = Path::new(&filename);
     let file = File::open(path)?;
@@ -30,64 +30,71 @@ fn load_config(config:&mut Config, filename:&str) -> Result<(),io::Error> {
 }
 
 fn main() -> io::Result<()> {
-   let matches = App::new("Pcap rewrite tool")
+    let matches = App::new("Pcap rewrite tool")
         .version(crate_version!())
         .author("Pierre Chifflier")
         .about("Tool for rewriting pcap files")
-        .arg(Arg::with_name("plugins")
-             .help("Plugins to load (default: all)")
-             .short("p")
-             .long("plugins")
-             .takes_value(true))
-        .arg(Arg::with_name("config")
-             .help("Configuration file")
-             .short("c")
-             .long("config")
-             .takes_value(true))
-        .arg(Arg::with_name("INPUT")
-             .help("Input file name")
-             .required(true)
-             .index(1))
-        .arg(Arg::with_name("OUTPUT")
-            .help("Output file name")
-            .required(true)
-            .index(2))
+        .arg(
+            Arg::with_name("plugins")
+                .help("Plugins to load (default: all)")
+                .short("p")
+                .long("plugins")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("config")
+                .help("Configuration file")
+                .short("c")
+                .long("config")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Input file name")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("Output file name")
+                .required(true)
+                .index(2),
+        )
         .get_matches();
 
-   // env_logger::init();
-   std_logger::init();
+    // env_logger::init();
+    std_logger::init();
 
-   debug!("Pcap rewrite tool {}", crate_version!());
+    debug!("Pcap rewrite tool {}", crate_version!());
 
-   let mut config = Config::default();
-   if let Some(filename) = matches.value_of("config") {
-       load_config(&mut config, filename)?;
-   }
+    let mut config = Config::default();
+    if let Some(filename) = matches.value_of("config") {
+        load_config(&mut config, filename)?;
+    }
 
-   let input_filename = matches.value_of("INPUT").unwrap();
-   let output_filename = matches.value_of("OUTPUT").unwrap();
+    let input_filename = matches.value_of("INPUT").unwrap();
+    let output_filename = matches.value_of("OUTPUT").unwrap();
 
-   let mut input_reader =
-       if input_filename == "-" {
-           Box::new(io::stdin()) as Box<io::Read>
-       } else {
-           let path = Path::new(&input_filename);
-           let file = File::open(path)?;
-           if input_filename.ends_with(".gz") {
-               Box::new(GzDecoder::new(file)) as Box<io::Read>
-           } else if input_filename.ends_with(".xz") {
-               Box::new(XzDecoder::new(file)) as Box<io::Read>
-           } else {
-               Box::new(file)
-           }
-       };
-   let path = Path::new(&output_filename);
-   let outfile = File::create(path)?;
+    let mut input_reader = if input_filename == "-" {
+        Box::new(io::stdin()) as Box<io::Read>
+    } else {
+        let path = Path::new(&input_filename);
+        let file = File::open(path)?;
+        if input_filename.ends_with(".gz") {
+            Box::new(GzDecoder::new(file)) as Box<io::Read>
+        } else if input_filename.ends_with(".xz") {
+            Box::new(XzDecoder::new(file)) as Box<io::Read>
+        } else {
+            Box::new(file)
+        }
+    };
+    let path = Path::new(&output_filename);
+    let outfile = File::create(path)?;
 
-   let rewriter = Rewriter::new(Box::new(outfile));
-   let mut engine = PcapEngine::new(Box::new(rewriter), &config);
+    let rewriter = Rewriter::new(Box::new(outfile));
+    let mut engine = PcapEngine::new(Box::new(rewriter), &config);
 
-   let _ = engine.run(&mut input_reader).expect("run analyzer");
+    let _ = engine.run(&mut input_reader).expect("run analyzer");
 
-   Ok(())
+    Ok(())
 }
