d42c2b10b4c2fae1a932c6265b81aec0304141cc
Johan Mazel <johan.mazel@ssi.gouv.fr>
2022-07-25 19:01:24 +0200

    Replace the triple callback in filter_utils by a single one for Ethernet and simplify code in keep function of DispatchFilter

--- pcap-rewrite/src/filters/dispatch_filter.rs
+++ pcap-rewrite/src/filters/dispatch_filter.rs
@@ -41,46 +41,35 @@ impl<C, D> DispatchFilter<C, D> {
     }
 
     pub fn keep<'j>(&self, packet_data: PacketData<'j>) -> FResult<PacketData<'j>, String> {
-        let keep = match packet_data {
+        let key = match packet_data {
             PacketData::L2(data) => {
                 if data.len() < 14 {
                     return Err("L2 data too small for ethernet".to_owned());
                 }
 
-                filter_utils::extract_test_callback_ethernet(
-                    &self.key_container,
+                filter_utils::extract_callback_ethernet(
                     &self.get_key_from_ipv4_l3_data,
                     &self.get_key_from_ipv6_l3_data,
-                    &self.keep,
                     data,
-                )
+                )?
             }
             PacketData::L3(l3_layer_value_u8, data) => {
                 let ether_type = EtherType::new(l3_layer_value_u8 as u16);
                 match ether_type {
-                    EtherTypes::Ipv4 => filter_utils::extract_test_callback_ipv4(
-                        &self.key_container,
-                        &self.get_key_from_ipv4_l3_data,
-                        &self.keep,
-                        data,
-                    ),
-                    EtherTypes::Ipv6 => filter_utils::extract_test_callback_ipv6(
-                        &self.key_container,
-                        &self.get_key_from_ipv6_l3_data,
-                        &self.keep,
-                        data,
-                    ),
+                    EtherTypes::Ipv4 => (self.get_key_from_ipv4_l3_data)(data)?,
+                    EtherTypes::Ipv6 => (self.get_key_from_ipv4_l3_data)(data)?,
                     _ => Err(format!(
                         "Unimplemented Ethertype in L3 {:?}/{:x}",
                         ether_type,
                         ether_type.to_primitive_values().0
-                    )),
+                    ))?,
                 }
             }
             PacketData::L4(_, _) => unimplemented!(),
             PacketData::Unsupported(_) => unimplemented!(),
         };
-        match keep {
+
+        match (&self.keep)(&self.key_container, &key) {
             Ok(b) => {
                 if b {
                     Ok(Verdict::Accept(packet_data))
