207aff7c49361e987aba10ab8ef233a7553d4a69
Pierre Chifflier <pierre.chifflier@ssi.gouv.fr>
2024-09-02 14:31:49 +0200

    test-analyzer: using `tracing` to send logs to output

--- test-analyzer/src/main.rs
+++ test-analyzer/src/main.rs
@@ -5,14 +5,15 @@ extern crate log;
 
 use clap::Parser;
 use explugin_example::ExEmptyPluginBuilder;
-use libpcap_analyzer::*;
 use libpcap_analyzer::plugins::PluginsFactory;
+use libpcap_analyzer::*;
 use libpcap_tools::{Config, PcapDataEngine, PcapEngine};
-use simplelog::{LevelFilter, SimpleLogger};
 use std::fs::File;
 use std::io;
 use std::path::Path;
 use std::sync::Arc;
+use tracing::Level;
+use tracing_subscriber::EnvFilter;
 
 use flate2::read::GzDecoder;
 use xz2::read::XzDecoder;
@@ -51,29 +52,19 @@ fn load_config(config: &mut Config, filename: &str) -> Result<(), io::Error> {
     config.load_config(file)
 }
 
-const ENV_LOG : &str = "PCAP_ANALYZER_LOG";
-fn env_get_log_level() -> LevelFilter {
-    match std::env::var(ENV_LOG) {
-        Ok(key) => {
-            match key.as_ref() {
-                "off" => LevelFilter::Off,
-                "error" => LevelFilter::Error,
-                "warn" => LevelFilter::Warn,
-                "info" => LevelFilter::Info,
-                "debug" => LevelFilter::Debug,
-                "trace" => LevelFilter::Trace,
-                _ => panic!("Invalid log level '{}'", key),
-            }
-        },
-        _ => LevelFilter::Debug
-    }
-}
-
 fn main() -> Result<(), io::Error> {
     let args = Args::parse();
 
-    let log_level = env_get_log_level();
-    let _ = SimpleLogger::init(log_level, simplelog::Config::default());
+    let env_filter = EnvFilter::try_from_env("PCAP_ANALYZER_LOG")
+        .unwrap_or_else(|_| EnvFilter::from_default_env().add_directive(Level::DEBUG.into()));
+    tracing_subscriber::fmt()
+        .with_env_filter(env_filter)
+        //.json()
+        //.with_span_events(FmtSpan::ENTER)
+        .with_thread_ids(true)
+        //.with_max_level(tracing::Level::TRACE)
+        .compact()
+        .init();
     info!("test-analyzer tool starting");
 
     // create plugin factory with all available plugins
@@ -94,15 +85,19 @@ fn main() -> Result<(), io::Error> {
     let registry = if let Some(plugin_names) = args.plugins {
         debug!("Restricting plugins to: {}", plugin_names);
         let names: Vec<_> = plugin_names.split(',').collect();
-        factory.build_filter_plugins(
-            |n| {
-                debug!("n: {}", n);
-                names.iter().any(|&x| n.contains(x))
-            },
-            &config,
-        ).expect("Could not build factory")
+        factory
+            .build_filter_plugins(
+                |n| {
+                    debug!("n: {}", n);
+                    names.iter().any(|&x| n.contains(x))
+                },
+                &config,
+            )
+            .expect("Could not build factory")
     } else {
-        factory.build_plugins(&config).expect("Could not build factory")
+        factory
+            .build_plugins(&config)
+            .expect("Could not build factory")
     };
     debug!("test-analyzer instantiated plugins:");
     registry.run_plugins(
@@ -111,16 +106,26 @@ fn main() -> Result<(), io::Error> {
             debug!("  {}", p.name());
             let t = p.plugin_type();
             let mut s = "    layers: ".to_owned();
-            if t & PLUGIN_L2 != 0 { s += "  L2"; }
-            if t & PLUGIN_L3 != 0 { s += "  L3"; }
-            if t & PLUGIN_L4 != 0 { s += "  L4"; }
+            if t & PLUGIN_L2 != 0 {
+                s += "  L2";
+            }
+            if t & PLUGIN_L3 != 0 {
+                s += "  L3";
+            }
+            if t & PLUGIN_L4 != 0 {
+                s += "  L4";
+            }
             debug!("{}", s);
             let mut s = "    events: ".to_owned();
-            if t & PLUGIN_FLOW_NEW != 0 { s += "  FLOW_NEW"; }
-            if t & PLUGIN_FLOW_DEL != 0 { s += "  FLOW_DEL"; }
+            if t & PLUGIN_FLOW_NEW != 0 {
+                s += "  FLOW_NEW";
+            }
+            if t & PLUGIN_FLOW_DEL != 0 {
+                s += "  FLOW_DEL";
+            }
             debug!("{}", s);
         },
-        );
+    );
 
     // let analyzer: Box<dyn PcapAnalyzer> = match config.get_usize("num_threads") {
     //     Some(1) => Box::new(Analyzer::new(registry, &config)),
@@ -162,19 +167,23 @@ fn main() -> Result<(), io::Error> {
 }
 
 fn show_results(analyzer: &Analyzer) {
-    analyzer.registry().run_plugins(|_| true, |p| {
-        let res = p.get_results();
-        // dbg!(&res);
-        if let Some(res) = res {
-            match p.name() {
-                "BasicStats" => display_json_basicstats(res),
-                "CommunityID" => display_json_communityid(res),
-                "FlowsInfo" => display_json_flowsinfo(res),
-                "Rusticata" => display_json_rusticata(res),
-                "TlsStats" => display_json_tlsstats(res),
-                _ => display_generic(p, res),
+    analyzer.registry().run_plugins(
+        |_| true,
+        |p| {
+            let res = p.get_results();
+            // dbg!(&res);
+            if let Some(res) = res {
+                match p.name() {
+                    "BasicStats" => display_json_basicstats(res),
+                    "CommunityID" => display_json_communityid(res),
+                    "FlowsInfo" => display_json_flowsinfo(res),
+                    "Rusticata" => display_json_rusticata(res),
+                    "TlsStats" => display_json_tlsstats(res),
+                    _ => display_generic(p, res),
+                }
+            } else {
+                info!("{}: <no value>", p.name());
             }
-        } else {
-            info!("{}: <no value>", p.name());
-        }    })
+        },
+    )
 }
